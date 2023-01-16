5639bcecf3ca0ea3679172cfc685b5d9447743e2
cambremnerr <cajbremner@gmail.com>
2023-01-16 18:24:52 -0500

    Error still not resolved

--- src/main/java/com/cam/Authentication/repository/RoleRepository.java
+++ src/main/java/com/cam/Authentication/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.cam.Authentication.repository;
 
-public class RoleRepository {
+import com.cam.Authentication.models.ERole;
+import com.cam.Authentication.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
